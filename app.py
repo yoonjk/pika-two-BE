14a9fbd570225be78d832aec56045ac56384ebc9
kangsan <tks0204@naver.com>
2022-08-09 13:18:59 +0900

    fear(config): add config file

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from flask import Flask
 from os import environ
+from config import DevConfig, PrdConfig
+
 
 app = Flask(__name__)
 
 if environ.get("FLASK_ENV") == "dev":
-    app.config.from_object("config.DevConfig")
+    app.config.from_object(DevConfig())
 else:
-    app.config.from_object("config.PrdConfig")
+    app.config.from_object(PrdConfig())
 
 @app.route('/')
 def hello():
-    msg = "DB_IP={}, debug={}".format(app.config["DB_IP"], app.config["DEBUG"])
+    msg = ""
+    for key, val in app.config["DB"].items():
+        msg += f"{key}={val}\n"
+    msg += f"{app.config['DB_URI']}"
     return msg
     
 if __name__ == '__main__':
     app.run(debug=app.config["DEBUG"], host="0.0.0.0", port=5000)
     print(app.config["DB_IP"])
+    
--- config.py
+++ config.py
@@ -3,17 +3,41 @@ from os import environ, path
 basedir = path.abspath(path.dirname(__file__))
 
 
-class Config:
+class Config(object):
     # Base Config
     DEBUG=True
-    # DB Config
-    DB_PORT=3306
 
+    # DB Config
+    DB = {
+        "user": "root",
+        "password": "", # 암호화 필요
+        "host": "",
+        "port": "3306",
+        "database": "",
+    }
+
+    @property
+    def DB_URI(self):
+        return f"mysql+pymysql://{self.DB['user']}:{self.DB['password']}@{self.DB['host']}:{self.DB['port']}/{self.DB['database']}"
+    
+    SQLALCHEMY_DATABASE_URI = DB_URI
 
 class DevConfig(Config):
     DEBUG=True
-    DB_IP="localhost"
+    DB = {
+        "user": "root",
+        "password": "", # 암호화 필요
+        "host": "localhost",
+        "port": "3306",
+        "database": "development",
+    }
 
 class PrdConfig(Config):
     DEBUG=False
-    DB_IP="0.0.0.0"    
+    DB = {
+        "user": "root",
+        "password": "", # 암호화 필요
+        "host": "0.0.0.0",
+        "port": "3306",
+        "database": "production",
+    }
