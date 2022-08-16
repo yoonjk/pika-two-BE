7d77a16fcea3314de933acd85a60d659f72a7463
kangsan <tks0204@naver.com>
2022-08-16 16:28:01 +0900

    feat(nickname_gen): mv nickname_gen method to random_gen.py

--- src/service/user.py
+++ src/service/user.py
@@ -1,6 +1,6 @@
 from src.database import db
 from src.model.models import User, FavoriteCompanies, Wage, Apply, JobPost, Company
-from src.service.nickname_gen import nick_gen
+from src.util.rand_gen import nick_gen
 from src.service.company import get_or_create_company
 import logging
 from datetime import date
